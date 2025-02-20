4a93f4ff35673904b9527662d9a444aa429b198e
rolandjuhasz <jszrl2003@gmail.com>
2025-02-20 16:29:36 +0100

    initial files

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Navbar from "./components/Navbar.vue";
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <Navbar />
+  <div class="bg-[#38BDF8]">
+    <RouterView />
+  </div>
+</template>
